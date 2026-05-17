b373d14c3cf573f1caacee1fcf60f2626189df43
Bolbatop <abdulahhmohammed71@gmail.com>
2026-05-17 13:05:37 +0300

    premissions

--- smart_home_app/lib/services/permission_service.dart
+++ smart_home_app/lib/services/permission_service.dart
@@ -4,24 +4,36 @@ import 'dart:developer' as developer;
 class PermissionService {
   static Future<bool> requestCameraPermission() async {
     PermissionStatus status = await Permission.camera.status;
-    developer.log('Camera Permission Status: $status', name: 'PermissionService');
+    developer.log(
+      'Camera Permission Status: $status',
+      name: 'PermissionService',
+    );
 
     if (status.isGranted) return true;
 
     status = await Permission.camera.request();
-    developer.log('Requested Camera Permission: $status', name: 'PermissionService');
-    
+    developer.log(
+      'Requested Camera Permission: $status',
+      name: 'PermissionService',
+    );
+
     return status.isGranted;
   }
 
   static Future<bool> requestGalleryPermission() async {
     PermissionStatus status = await Permission.photos.status;
-    developer.log('Gallery Permission Status: $status', name: 'PermissionService');
+    developer.log(
+      'Gallery Permission Status: $status',
+      name: 'PermissionService',
+    );
 
     if (status.isGranted) return true;
 
     status = await Permission.photos.request();
-    developer.log('Requested Gallery Permission: $status', name: 'PermissionService');
+    developer.log(
+      'Requested Gallery Permission: $status',
+      name: 'PermissionService',
+    );
 
     return status.isGranted;
   }
